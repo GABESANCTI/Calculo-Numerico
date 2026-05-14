047de02b03f02cc9b3fda0aaae3b101ace906283
GABESANCTI <gabesancti@gmail.com>
2026-05-14 18:34:30 -0400

    FEAT(RLC): Implementação com o exercicio proposto sobre um circuito RLC, o exercicio proposto está em PDF assim como parte do enunciado está comentado no código.

--- rlc.py
+++ rlc.py
@@ -4,7 +4,7 @@ import math
 envolver a determinação do resistor apropriado para dissipar energia a uma taxa
 específica , com valores conhecidos para L e C. Para esse Problema, suponha que a
 carga deve ser dissipada a 1% do seu valor original (q/q0 = 0,01) em t=0,05s, com L = 5
-H eC=10-4F
+H e C=10(exp)-4F
 PS: Use o método da Bissecção. O método de Newton-Raphson pode ser incoveniente
 em virtude do cálculo da derivada de (1) ser trabalhoso.
 Avariável implícita da equação é R. '''
@@ -15,8 +15,8 @@ t = 0.05
 alvo = 0.01         
 
 
-a = 0.0            # Limite inferior de Resistência (R)
-b = 400.0          # Limite superior de Resistência (R)
+a = 300           # Limite inferior de Resistência (R)
+b = 400          # Limite superior de Resistência (R)
 tol = 0.0001      
 
 
@@ -35,7 +35,6 @@ else:
         
         print(f"{i:<5} | {p:<18.6f} | {(b-a)/2:<15.6f} | [{a:.6f}, {b:.6f}]")
         
-        # Teste de sinal para decidir o próximo intervalo [cite: 92, 94]
         if f(a) * f(p) < 0:
             b = p  
         else:
